c809f295d53b6f0a4a672f2e36f18d192dca0900
K112p1Esc <matheus.fsilva@sptech.school>
2026-04-23 13:29:03 -0300

    Colocando FOR ao invés de FOREACH

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,46 +1,74 @@
-// Seleciona TODAS as imagens do carousel (classe .slide)
-// Para então virar uma NODE LIST (coleção de objetos para usar o querySelectorALl) com o document (seleciona a tag)
+// // Seleciona TODAS as imagens do carousel (classe .slide)
+// // Para então virar uma NODE LIST (coleção de objetos para usar o querySelectorALl) com o document (seleciona a tag)
+// let slides = document.querySelectorAll(".slide");
+// // Seleciona todos os indicadores (que ta como indica, a bolinha!)
+// let bolinha = document.querySelectorAll(".indica");
+// // É o índice da primeira imagem (que é 0 né)
+// let i = 0;
+
+
+
+// // Função que avança para o próximo slide
+// function nextSlide() {
+//     // Aumenta o meu indice
+//     i++;
+//     // Ve se passou da última imagem para então voltar a primeira
+//     if (i >= slides.length) {
+//         // O loop ai
+//         i = 0;
+//     }
+//     // Mostra o slide atual invocando a função
+//     showSlide(i);
+// }
+
+// // Executa a função nextSlide a cada 3 segundos, que é 3000 ms em JS
+// // Ai fica parecendo que é automatico
+// setInterval(nextSlide, 4000);
+
+// // Função que mostra o slide
+// function showSlide(i) {
+
+//     // Remove a classe "active" de TODAS as imagens
+//     slides.forEach(slide => slide.classList.remove("active"));
+
+//     // Faz o mesmo que nas imagens só que com as bolinhas
+//     bolinha.forEach(indica => indica.classList.remove("active"));
+
+//     // Adiciona "active" SOMENTE na imagem atual (se baseando no indice)
+//     // Isso faz ela aparecer (por causa do CSS e JS)
+//     slides[i].classList.add("active");
+//     // Ativa também a bolinha 
+//     bolinha[i].classList.add("active");
+// }
+
 let slides = document.querySelectorAll(".slide");
-// Seleciona todos os indicadores (que ta como indica, a bolinha!)
-let dots = document.querySelectorAll(".indica");
-// É o índice da primeira imagem (que é 0 né)
-let index = 0;
+let bolinha = document.querySelectorAll(".indica");
+let i = 0;
 
-
-
-// Função que avança para o próximo slide
 function nextSlide() {
-    // Aumenta o meu indice
-    index++;
-    // Ve se passou da última imagem para então voltar a primeira
-    if (index >= slides.length) {
-        // O loop ai
-        index = 0;
+    i++;
+    if (i >= slides.length) {
+        i = 0;
     }
-    // Mostra o slide atual invocando a função
-    showSlide(index);
+    showSlide(i);
 }
 
+setInterval(nextSlide, 4000);
 
-
-// Executa a função nextSlide a cada 3 segundos, que é 3000 ms em JS
-// Ai fica parecendo que é automatico
-setInterval(nextSlide, 3000);
-
-
-
-// Função que mostra o slide
 function showSlide(i) {
+    // Remove "active" de todos os slides
+    for (let j = 0; j < slides.length; j++) {
+        slides[j].classList.remove("active");
+    }
 
-    // Remove a classe "active" de TODAS as imagens
-    slides.forEach(slide => slide.classList.remove("active"));
+    // Remove "active" de todos os bolinha
+    for (let j = 0; j < bolinha.length; j++) {
+        bolinha[j].classList.remove("active");
+    }
 
-    // Faz o mesmo que nas imagens só que com as bolinhas
-    dots.forEach(indica => indica.classList.remove("active"));
-
-    // Adiciona "active" SOMENTE na imagem atual (se baseando no indice)
-    // Isso faz ela aparecer (por causa do CSS e JS)
+    // Ativa o slide atual
     slides[i].classList.add("active");
-    // Ativa também a bolinha 
-    dots[i].classList.add("active");
+
+    // Ativa o dot atual
+    bolinha[i].classList.add("active");
 }
