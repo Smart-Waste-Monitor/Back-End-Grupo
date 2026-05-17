a31bd314bb0d1d35aff8efbb48efbd1aeb88d481
K112p1Esc <matheus.fsilva@sptech.school>
2026-05-17 15:07:26 -0300

    Feat: Arrumando login

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -17,12 +17,14 @@ function autenticar(req, res) {
                 function (resultadoAutenticar) {
                     console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
                     console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
-                    if (resultadoAutenticar.length == 0) {
-                                    res.status(403).send("Email e/ou senha inválido(s)");
-                                } else {
-                                    res.status(403).send("Mais de um usuário com o mesmo login e senha!");
-                                }
-                            }
+                    if (resultadoAutenticar.length == 1) {
+                        res.status(200).json(resultadoAutenticar[0]);
+                    } else if (resultadoAutenticar.length == 0) {
+                        res.status(403).send("Email e/ou senha inválido(s)");
+                    } else {
+                        res.status(403).send("Mais de um usuário com o mesmo login e senha!");
+                    }
+                }
             ).catch(
                 function (erro) {
                     console.log(erro);
@@ -44,7 +46,7 @@ function cadastrar(req, res) {
     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {  
+    } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
