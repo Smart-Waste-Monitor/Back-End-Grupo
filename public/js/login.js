3bdbc65755ccd2fba6a968200960c77d1f8f3892
Heloisa de Almeida Oliveira <heloisa.a200607@gmail.com>
2026-05-22 00:01:03 -0300

    update login

--- public/js/login.js
+++ public/js/login.js
@@ -31,31 +31,31 @@ function entrar() {
         if (resposta.ok) {
             console.log(resposta);
 
-            resposta.json().then(json => {
-                console.log(json);
-                console.log(JSON.stringify(json));
-                sessionStorage.EMAIL_USUARIO = json.email;
-                sessionStorage.NOME_USUARIO = json.nome;
-                sessionStorage.ID_USUARIO = json.idUsuario;
+                resposta.json().then(json => {
+                    sessionStorage.setItem('usuario', JSON.stringify({ // Cria objeto json para verificar
+                        id: json.idUsuario,
+                        email: json.email,
+                        nome: json.nome,
+                        fk: json.fkHospital
+                    }))
+                    setTimeout(function () {
+                        window.location = "./index.html";
+                    }, 1000); // apenas para exibir o loading
 
-                setTimeout(function () {
-                    window.location = "./index.html";
-                }, 1000); // apenas para exibir o loading
+                });
 
-            });
+            } else {
 
-        } else {
+                console.log("Houve um erro ao tentar realizar o login!");
 
-            console.log("Houve um erro ao tentar realizar o login!");
-
-            resposta.text().then(texto => {
-                console.error(texto);
-            });
-        }
+                resposta.text().then(texto => {
+                    console.error(texto);
+                });
+            }
 
     }).catch(function (erro) {
-        console.log(erro);
-    })
+                console.log(erro);
+            })
 
     return false;
 }
