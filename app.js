744bf8397024121c3f52708195b03290c92b65e9
Letícia dos Santos Alves <leticia.alves@sptech.school>
2026-05-28 15:53:09 -0300

    feat: Iniciação da implementação do Bob IA.

--- app.js
+++ app.js
@@ -7,13 +7,24 @@ var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
 
 require("dotenv").config({ path: caminho_env });
 
-var express = require("express");
+//Importação Bob IA
+const { GoogleGenAI } = require("@google/genai");
+const express = require("express");
+const path = require("path");
+
+// carregando as variáveis de ambiente do projeto do arquivo .env
+require("dotenv").config();
+
 var cors = require("cors");
-var path = require("path");
 var PORTA_APP = process.env.APP_PORT;
 var HOST_APP = process.env.APP_HOST;
 
-var app = express();
+//configurando o servidor Bob IA
+const app = express();
+const PORTA_SERVIDOR = process.env.PORTA;
+// configurando o gemini (IA)
+const chatIA = new GoogleGenAI({ apiKey: process.env.MINHA_CHAVE });
+
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
@@ -33,6 +44,20 @@ app.use("/usuarios", usuarioRouter);
 //app.use("/medidas", medidasRouter);
 app.use("/hospital", hospitalRouter);
 
+
+// configurando o servidor para receber requisições JSON BOB IA
+app.use(express.json());
+
+// configurando o servidor para servir arquivos estáticos BOB IA
+app.use(express.static(path.join(__dirname, "public")));
+
+// configurando CORS BOB IA
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+    next();
+});
+
 app.listen(PORTA_APP, function () {
     console.log(`
     ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
@@ -47,3 +72,39 @@ app.listen(PORTA_APP, function () {
     .:${process.env.AMBIENTE_PROCESSO}:.
     ===================================================================`);
 });
+
+// rota para receber perguntas e gerar respostas
+app.post("/perguntar", async (req, res) => {
+    const pergunta = req.body.pergunta;
+
+    try {
+        const resultado = await gerarResposta(pergunta);
+        res.json({ resultado });
+    } catch (error) {
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+
+});
+
+// função para gerar respostas usando o gemini
+async function gerarResposta(mensagem) {
+
+    try {
+        // gerando conteúdo com base na pergunta
+        const modeloIA = chatIA.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents: `Em um paragráfo responda: ${mensagem}`
+
+        });
+        const resposta = (await modeloIA).text;
+        const tokens = (await modeloIA).usageMetadata;
+
+        console.log(resposta);
+        console.log("Uso de Tokens:", tokens);
+
+        return resposta;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
